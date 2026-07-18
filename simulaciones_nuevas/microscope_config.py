9a32263e483b64d3a688df9a99343a187119679d
Ale <nahueltoranzo283@gmail.com>
2026-07-18 14:02:49 -0300

    Added spherical illumination functions.

--- simulaciones_nuevas/microscope_config.py
+++ simulaciones_nuevas/microscope_config.py
@@ -1,40 +1,72 @@
 from common import *
 
-matrix_center = (0,0,0)
-leds_number_x = 31
-leds_number_y = 31
-central_led = (leds_number_x + 1) // 2, (leds_number_y + 1) // 2
-
-sample_height = 76e-3
+matrix_center = (0,0,91e-3)
+sample_height = 0
 sample_position = (0, 0, sample_height)
 
-numerical_aperture = 0.1
-wavelength = 6.8e-7 #4.7e-07: "blue" , 5.3e-07: "green", 6.8e-07: "red"
+numerical_aperture = 0.07
+wavelength = 6e-7 #4.7e-07: "blue" , 5.3e-07: "green", 6.8e-07: "red"
 pixel_size = 3.2e-6
-magnification = 2
+magnification = 1
 
 
 """Fourier Pixel Factor para la configuración que tenemos y un HR_shape de 512x512"""
 
 led_equiespaciados = True
+brazo = True
 
-if led_equiespaciados:
-    led_spacing = 6e-3
-    led_positions = calculate_led_positions_equi(leds_number_x, leds_number_y, led_spacing, matrix_center)
-else:
-    #Calculamos el k_spacing teniendo en cuenta la cantidad de leds como k_spacing = 2*np.pi()*fourier_pixe_factor*(512/33) aprox un k_spacing en pixeles de 15
-    k_spacing = 405000
-    k_distributions = calculate_led_positions_equi(leds_number_x, leds_number_y, k_spacing, matrix_center)
-    led_positions = calculate_led_positions_no_equi(k_distributions, central_led, sample_height, wavelength, matrix_center)
+radio_brazo = 9.1 * 10**(-2)
+n_leds_brazo = 9
+overlap = 40
+
+if brazo:
+    central_led = (1, 1)
+
+    max_theta = 70 * (np.pi/180)
+    max_angle = max_theta
+
+    ratio_LR = calculate_LR_ratio(numerical_aperture, max_angle)
+
+    NA_synth = 1 / np.sqrt(1+(1/np.tan(max_angle))**2)
+    number_pixels_HR = 512
+    ratio_LR_synt = numerical_aperture / NA_synth 
+    number_pixels_LR = number_pixels_HR * ratio_LR_synt
+    fourier_pixel_factor = magnification / (number_pixels_LR * pixel_size)
     
-max_angle = calulate_max_angle(led_positions, central_led, sample_height)
-ratio_LR = calculate_LR_ratio(numerical_aperture, max_angle)
+    paso_theta = max_angle / (n_leds_brazo - 1)
+    thetas = np.arange(0 + paso_theta, max_theta+paso_theta, paso_theta)
+    #thetas = np.delete(thetas, [-3])
 
-NA_synth = 1 / np.sqrt(1+(1/np.tan(max_angle))**2)
-number_pixels_HR = 512
-ratio_LR_synt = numerical_aperture / NA_synth
-number_pixels_LR = number_pixels_HR * ratio_LR_synt
-fourier_pixel_factor = magnification / (number_pixels_LR * pixel_size)
+    angulos_motor = circunf_max_equi_overlap(overlap, 37 // 2, max_theta, wavelength, fourier_pixel_factor) 
+    led_positions = calculate_led_positions_brazo_equi(overlap, 37 // 2, angulos_motor,
+                                                       thetas, radio_brazo, wavelength, fourier_pixel_factor)
+    max_angle = calulate_max_angle(led_positions, central_led, sample_height)            
+    leds_number_x = 250
+    leds_number_y = 250
+else: 
+    leds_number_x = 31
+    leds_number_y = 31
+    central_led = (leds_number_x + 1) // 2, (leds_number_y + 1) // 2
+
+    if led_equiespaciados:
+        led_spacing = 6e-3 
+        led_positions = calculate_led_positions_equi(leds_number_x, leds_number_y, led_spacing, matrix_center)
+    else:
+        #Calculamos el k_spacing teniendo en cuenta la cantidad de leds como k_spacing = 2*np.pi()*fourier_pixe_factor*(512/33) aprox un k_spacing en pixeles de 15
+        # Recalcular para el brazo
+        k_spacing = 405000
+        k_distributions = calculate_led_positions_equi(leds_number_x, leds_number_y, k_spacing, matrix_center)
+        led_positions = calculate_led_positions_no_equi(k_distributions, central_led, sample_height, wavelength, matrix_center)
+        
+    max_angle = calulate_max_angle(led_positions, central_led, sample_height)            
+    ratio_LR = calculate_LR_ratio(numerical_aperture, max_angle)
+    
+    NA_synth = 1 / np.sqrt(1+(1/np.tan(max_angle))**2)
+    number_pixels_HR = 512
+    ratio_LR_synt = numerical_aperture / NA_synth
+    number_pixels_LR = number_pixels_HR * ratio_LR_synt
+    fourier_pixel_factor = magnification / (number_pixels_LR * pixel_size)
+    
 ## Los valores deben estar en mm y deben estar entre -5e-3 y 5e-3 
 h_error = 0
 x_offset = 0
